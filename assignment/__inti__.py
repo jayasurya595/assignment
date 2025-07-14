45c4afe5a394b9af32a2fa2915934f12882a0cdd
jayasurya immidi <78395712+jayasurya595@users.noreply.github.com>
2025-07-14 14:27:14 +0530

    Add files via upload

---
+++ assignment/__inti__.py
@@ -0,0 +1,2 @@
+from intro import introduction
+
---
+++ assignment/intro.py
@@ -0,0 +1,5 @@
+def introduction():
+    print("My name is jayasurya")
+    print("I graduated from LPU")
+    print("Looking forward to working with you")
+
